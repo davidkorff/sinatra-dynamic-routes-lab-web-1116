aaff88fe5ce44706151e94cc610aef606bd1128b
github username <github email address>
2016-12-20 15:51:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,43 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    square = params[:number].to_i*params[:number].to_i
+    square.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    #binding.pry
+    @string = ''
+    params[:number].to_i.times do
+      @string += "#{params[:phrase]}\n"
+    end
+     "#{@string}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    #binding.pry
+    array=[params[:word1],params[:word2],params[:word3],params[:word4],params[:word5]]
+    "#{array.join(" ")}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    if params[:operation]== 'add'
+      "#{params[:number1].to_i+params[:number2].to_i}"
+    elsif params[:operation]== 'subtract'
+      "#{params[:number1].to_i-params[:number2].to_i}"
+    elsif params[:operation]=='multiply'
+      "#{params[:number1].to_i*params[:number2].to_i}"
+    elsif params[:operation]=='divide'
+      "#{params[:number1].to_i/params[:number2].to_i}"
+    else
+      'fuck off'
+    end
+  end
+
+
 end
